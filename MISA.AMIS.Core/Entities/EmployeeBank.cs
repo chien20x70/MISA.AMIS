bd99e0bf5aa7d99767851427e995ba01c682e663
Chiến Nguyễn <chien20x70@gmail.com>
2021-05-06 17:31:05 +0700

    bank

---
+++ MISA.AMIS.Core/Entities/EmployeeBank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.AMIS.Core.Entities
+{
+    public class EmployeeBank
+    {
+        public Guid EmployeeBankId { get; set; }
+        public string BankName { get; set; }
+        public string BanhNumber { get; set; }
+        public string Branch { get; set; }
+        public string BankCity { get; set; }
+        public int Status { get; set; }
+        public Guid EmployeeId { get; set; }
+        public string StatusName { get; set; }
+
+    }
+}
